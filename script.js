7eaa06db85e9689c8771d92a9427671334f398e9
Rose <rosebeats09@gmail.com>
2023-08-05 19:47:35 -0400

    Create functions for basic math operators

---
+++ script.js
@@ -0,0 +1,22 @@
+// Functions for basic math operators
+
+// add
+function add(a, b) {
+    return a + b;
+}
+// substract 
+function subtract (a, b) {
+    return a - b;
+}
+// multiply 
+function multiply (a, b) {
+    return a * b;
+}
+//divide 
+function divide (a, b) {
+    return a / b;
+}
+console.log(add(2, 2));
+console.log(subtract(2, 2));
+console.log(multiply(2, 2));
+console.log(divide(2, 2));
